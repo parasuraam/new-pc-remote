c967d62222b1040a19e84893c41f42a3da5c648e
parasuraam <67687893+parasuraam@users.noreply.github.com>
2021-02-17 14:45:00 +0000

    Add files via upload

---
+++ abcdefgh/app/src/main/java/com/example/abcdefgh/SecondActivity.java
@@ -0,0 +1,52 @@
+package com.example.abcdefgh;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.abcdefgh.server.Server;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private TextView info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        info = findViewById(R.id.terv);
+
+        new Thread(new Te()).start();
+    }
+
+    private class Te implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+
+                        Activity activity = null;
+                        final StringBuilder message = (StringBuilder) Server.getInstance(activity).objectInputStream.readObject();
+                        if (message != null) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    info.append(message);
+                                }
+                            });
+                        }
+
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
